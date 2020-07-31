fece38d452e8056b25ba135bea541dba92db57e3
boyan <yangbinyhbn@gmail.com>
2020-07-31 23:21:44 +0800

    add submodule update note

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,4 +3,15 @@ rootProject.name = "check-apk-words"
 include("words-lib")
 include("words-gradle-plugin")
 
-includeBuild("./thirdparty/Apktool")
+val apkToolProjectPath = "./thirdparty/Apktool"
+if (!File(apkToolProjectPath).exists()) {
+    throw IllegalStateException("""
+        
+        file `./thirdparty/Apktool` do not exists.
+        please update your git submodule first.
+        
+        git submodule update
+        
+    """.trimIndent())
+}
+includeBuild(apkToolProjectPath)
